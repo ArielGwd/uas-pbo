f3834bfa7682079126591ce33af2ac2f0130f1c7
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-02-12 18:55:35 +0700

    feat: association & composition

---
+++ Association.java
@@ -0,0 +1,43 @@
+class Mahasiswa {
+    private String nama;
+
+    public Mahasiswa(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void display() {
+        System.out.println("Mahasiswa: " + nama);
+    }
+}
+
+class Dosen {
+    private String nama;
+
+    public Dosen(String nama) {
+        this.nama = nama;
+    }
+
+    public void display() {
+        System.out.println("Dosen: " + nama + "\n");
+    }
+
+    public void mengajar(Mahasiswa mhs) {
+        System.out.println("Dosen " + nama + " mengajar mahasiswa " + mhs.getNama() + "\n");
+    }
+}
+
+public class Association {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa("Ariel");
+        Dosen dosen = new Dosen("Dr. Anwar");
+
+        mhs.display();
+        dosen.display();
+
+        dosen.mengajar(mhs);
+    }
+}
---
+++ Composition.java
@@ -0,0 +1,30 @@
+class Engine {
+    private String type;
+
+    public Engine(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
+
+class Mobil {
+    private Engine engine;
+
+    public Mobil(String engineType) {
+        this.engine = new Engine(engineType);
+    }
+
+    public void showEngineType() {
+        System.out.println("Tipe Mesin Mobil : " + engine.getType());
+    }
+}
+
+public class Composition {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil("V8");
+        mobil.showEngineType();
+    }
+}
