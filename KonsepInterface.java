982cf514716398f99ecc7fdb7d5b7bf9e07a295a
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-02-12 21:52:29 +0700

    feat: add interface

---
+++ KonsepInterface.java
@@ -0,0 +1,31 @@
+interface pekerja {
+    void bekerja();
+    void gaji();
+}
+
+class Karyawan implements pekerja {
+    private String nama;
+    private int gaji;
+
+    public Karyawan(String nama, int gaji) {
+        this.nama = nama;
+        this.gaji = gaji;
+    }
+
+    public void bekerja() {
+        System.out.println("Karyawan " + nama + " sedang bekerja");
+    }
+
+    public void gaji() {
+        System.out.println("Gaji Karyawan " + nama + " adalah " + gaji);
+    }
+}
+
+
+public class KonsepInterface {
+    public static void main(String[] args) {
+        Karyawan karyawan = new Karyawan("Joko", 5000000);
+        karyawan.bekerja();
+        karyawan.gaji();
+    }
+}
